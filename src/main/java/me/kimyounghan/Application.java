fff87df7d4d141748a479964c7afdddaa41c65a3
KIMYOUNGHAN <yh22953905@gmail.com>
2020-04-26 21:36:43 +0900

    스프링 부트 개념과 활용 3부 스프링 부트 원리 > 내장 웹 서버 응용 1부 : 컨테이너와 포트

--- src/main/java/me/kimyounghan/Application.java
+++ src/main/java/me/kimyounghan/Application.java
@@ -3,14 +3,11 @@ package me.kimyounghan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+        SpringApplication.run(Application.class, args);
     }
 }
