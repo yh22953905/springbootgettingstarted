4f654109c1c929e8527a00a85bd85be91ba7ff4a
KIMYOUNGHAN <yh22953905@gmail.com>
2020-04-28 00:39:13 +0900

    스프링 부트 개념과 활용 3부 스프링 부트 원리 > 내장 웹 서버 응용 2부 : HTTP2 설정

--- src/main/java/me/kimyounghan/Application.java
+++ src/main/java/me/kimyounghan/Application.java
@@ -1,11 +1,7 @@
 package me.kimyounghan;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,19 +12,6 @@ public class Application {
     @GetMapping("/hello")
     public String hello() {
         return "Hello Spring";
-    }
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8080);
-        return connector;
     }
 
     public static void main(String[] args) {
