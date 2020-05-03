2a270637ee34115327ea7c121be0c17e8e9bea33
KIMYOUNGHAN <yh22953905@gmail.com>
2020-05-03 21:11:31 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > SpringApplication 2부 - 3

--- src/main/java/me/kimyounghan/SampleListener.java
+++ src/main/java/me/kimyounghan/SampleListener.java
@@ -1,12 +1,14 @@
 package me.kimyounghan;
 
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SampleListener {
-    public SampleListener(ApplicationArguments arguments) {
-        System.out.println("foo : " + arguments.containsOption("foo"));
-        System.out.println("bar : " + arguments.containsOption("bar"));
+public class SampleListener implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("foo : " + args.containsOption("foo"));
+        System.out.println("bar : " + args.containsOption("bar"));
     }
 }
