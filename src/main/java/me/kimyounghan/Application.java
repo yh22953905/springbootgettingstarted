2a23d7a2ee20ef3b535ba48f6385093d061fcbdb
KIMYOUNGHAN <yh22953905@gmail.com>
2020-05-03 20:22:38 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > SpringApplication 1부

--- src/main/java/me/kimyounghan/Application.java
+++ src/main/java/me/kimyounghan/Application.java
@@ -1,20 +1,24 @@
 package me.kimyounghan;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello Spring";
-    }
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+                out.println("==========");
+                out.println("CLASSBANNER");
+                out.println("==========");
+            }
+        });
+        app.run(args);
     }
 }
