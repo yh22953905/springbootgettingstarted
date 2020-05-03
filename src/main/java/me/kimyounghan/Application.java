794fea6f4bf31d9df55e962227ee2a607a52bc2a
KIMYOUNGHAN <yh22953905@gmail.com>
2020-05-03 20:53:24 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > SpringApplication 2부 - 1

--- src/main/java/me/kimyounghan/Application.java
+++ src/main/java/me/kimyounghan/Application.java
@@ -1,24 +1,13 @@
 package me.kimyounghan;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-
-import java.io.PrintStream;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-        app.setBanner(new Banner() {
-            @Override
-            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
-                out.println("==========");
-                out.println("CLASSBANNER");
-                out.println("==========");
-            }
-        });
+        app.addListeners(new SampleListener());
         app.run(args);
     }
 }
---
+++ src/main/java/me/kimyounghan/SampleListener.java
@@ -0,0 +1,13 @@
+package me.kimyounghan;
+
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+
+public class SampleListener implements ApplicationListener<ApplicationStartingEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent applicationStartingEvent) {
+        System.out.println("=======================");
+        System.out.println("Application is starting");
+        System.out.println("=======================");
+    }
+}
