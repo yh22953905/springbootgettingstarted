86bc780be2164d7c4eb31efa67e25b27a4530c37
KIMYOUNGHAN <yh22953905@gmail.com>
2020-05-04 15:39:41 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > 외부 설정 1부

--- src/main/java/me/kimyounghan/Application.java
+++ src/main/java/me/kimyounghan/Application.java
@@ -1,14 +1,12 @@
 package me.kimyounghan;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-        app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
     }
 }
---
+++ src/main/java/me/kimyounghan/SampleRunner.java
@@ -0,0 +1,23 @@
+package me.kimyounghan;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleRunner implements ApplicationRunner {
+    @Value("${younghan.full.name}")
+    private String name;
+
+    @Value("${younghan.age}")
+    private int age;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("==================================================");
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println("==================================================");
+    }
+}
