0c09c8fa6bc851a6aa1e906b63e903ab01353c77
KIMYOUNGHAN <yh22953905@gmail.com>
2020-05-04 20:37:30 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > 테스트 - 3 @SpringBootTest RANDOM_PORT(WebTestClient)

--- src/test/java/me/kimyounghan/sample/SampleControllerTest.java
+++ src/test/java/me/kimyounghan/sample/SampleControllerTest.java
@@ -5,17 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerTest {
     @Autowired
-    TestRestTemplate testRestTemplate;
+    WebTestClient webTestClient;
 
     @MockBean
     SampleService mockSampleService;
@@ -24,7 +23,8 @@ public class SampleControllerTest {
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("01");
 
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello 01");
+        webTestClient.get().uri("/hello").exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo("hello 01");
     }
 }
