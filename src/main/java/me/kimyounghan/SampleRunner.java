a1432f45c6f30972345146215c6204405fce2825
KIMYOUNGHAN <yh22953905@gmail.com>
2020-05-04 16:53:43 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > 외부 설정 2부 (1) (2)

--- src/main/java/me/kimyounghan/SampleRunner.java
+++ src/main/java/me/kimyounghan/SampleRunner.java
@@ -1,23 +1,21 @@
 package me.kimyounghan;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
-    @Value("${younghan.full.name}")
-    private String name;
-
-    @Value("${younghan.age}")
-    private int age;
+    @Autowired
+    YounghanProperties younghanProperties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("==================================================");
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println(younghanProperties.getFullName());
+        System.out.println(younghanProperties.getAge());
+        System.out.println(younghanProperties.getSessionTimeout());
         System.out.println("==================================================");
     }
 }
