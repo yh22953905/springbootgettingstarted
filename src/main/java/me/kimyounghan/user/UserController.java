9e91556c4e24c189e5d1948ab8fe2e3ad639baae
KIMYOUNGHAN <yh22953905@gmail.com>
2020-05-05 20:20:29 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > 스프링 웹 MVC 2부: HttpMessageConverters

--- src/main/java/me/kimyounghan/user/UserController.java
+++ src/main/java/me/kimyounghan/user/UserController.java
@@ -1,13 +1,16 @@
 package me.kimyounghan.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
-
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+        return user;
     }
 }
