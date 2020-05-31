168a900c8937c8bbb40fd12e8790394fcb6d54e3
KIMYOUNGHAN <yh22953905@gmail.com>
2020-05-31 22:41:13 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > 스프링 데이터 2부: 인메모리 데이터베이스

--- src/main/java/me/kimyounghan/Application.java
+++ src/main/java/me/kimyounghan/Application.java
@@ -2,21 +2,9 @@ package me.kimyounghan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-//    @CrossOrigin(origins = "http://localhost:18080")
 public class Application {
-//    @CrossOrigin(origins = "http://localhost:18080")
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
-
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
