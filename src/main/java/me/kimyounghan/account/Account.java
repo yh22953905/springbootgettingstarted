9a3ab42db31faed59da1a51635a1e8952499d2b5
KIMYOUNGHAN <yh22953905@gmail.com>
2020-06-07 19:02:40 +0900

    스프링 부트 개념과 활용 4부 스프링 부트 활용 > 스프링 데이터 7부: 데이터베이스 초기화

--- src/main/java/me/kimyounghan/account/Account.java
+++ src/main/java/me/kimyounghan/account/Account.java
@@ -14,6 +14,8 @@ public class Account {
 
     private String password;
 
+    private String email;
+
     public Long getId() {
         return id;
     }
@@ -36,6 +38,14 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
